3bf78e1080d717a50f34aa7c90174caa45a4e834
iaburton <iaburton@users.noreply.github.com>
2019-01-08 15:20:41 -0600

    EncryptedEditText: Modify some methods and remove some logs

--- app/src/main/java/com/alloypowered/cstation/EncryptedEditTextPreference.java
+++ app/src/main/java/com/alloypowered/cstation/EncryptedEditTextPreference.java
@@ -3,11 +3,10 @@ package com.alloypowered.cstation;
 import android.content.Context;
 import androidx.preference.EditTextPreference;
 import android.util.AttributeSet;
-import android.util.Log;
+//import android.util.Log;
 
 //Test class and methods for the moment
 public class EncryptedEditTextPreference extends EditTextPreference {
-
     private static final String TAG = "CustomETP";
 
     public EncryptedEditTextPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -15,15 +14,15 @@ public class EncryptedEditTextPreference extends EditTextPreference {
     }
 
     public EncryptedEditTextPreference(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        this(context, attrs, defStyleAttr, 0);
     }
 
     public EncryptedEditTextPreference(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, R.attr.editTextPreferenceStyle);
     }
 
     public EncryptedEditTextPreference(Context context) {
-        super(context);
+        this(context, null);
     }
 
 //    @Override
@@ -41,25 +40,19 @@ public class EncryptedEditTextPreference extends EditTextPreference {
     public String getText() {
         String saved = super.getText();
         if (saved == null || saved.isEmpty()) {
-            Log.i(TAG, "in getText: null str");
             return saved;
         }
 
-        String toReturn = new StringBuilder(saved).reverse().toString();
-        Log.i(TAG, "in getText: saved (" + saved + ") returning (" + toReturn +")");
-        return toReturn;
+        return CryptoHelper.decryptText(saved);
     }
 
     @Override
     public void setText(String text) {
         if (text == null || text.isEmpty()) {
-            Log.i(TAG, "in setText: null str");
             super.setText(text);
             return;
         }
 
-        String toSave = new StringBuilder(text).reverse().toString();
-        Log.i(TAG, "in setText: set (" + text + ") saving (" + toSave +")");
-        super.setText(toSave);
+        super.setText(CryptoHelper.encryptText(text));
     }
 }
